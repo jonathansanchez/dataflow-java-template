81906d34840b1da17f3fca10f4c32ca7ef5eeaed
Jonathan Sánchez <jsanchezorellana@gmail.com>
2025-01-17 19:47:29 -0300

    feat: Dependencies are updated simplified

--- src/main/java/com/verix/example/Application.java
+++ src/main/java/com/verix/example/Application.java
@@ -1,13 +1,13 @@
 package com.verix.example;
 
-import org.springframework.boot.SpringApplication;
+/*import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication*/
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		//SpringApplication.run(Application.class, args);
 		DataPipeline.main(args);
 	}
 
