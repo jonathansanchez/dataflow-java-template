e83a6207f4c358fedf091d39c6d8d8c93b916795
Jonathan Sánchez <jsanchezorellana@gmail.com>
2025-01-22 11:41:59 -0300

    feat: Container Dependencies was updated with BigQuery Schema creator service.

--- src/main/java/com/verix/sam/infrastructure/config/DIContainer.java
+++ src/main/java/com/verix/sam/infrastructure/config/DIContainer.java
@@ -1,9 +1,11 @@
 package com.verix.sam.infrastructure.config;
 
+import com.google.api.services.bigquery.model.TableSchema;
 import com.verix.sam.application.service.ReaderService;
 import com.verix.sam.application.service.WriterService;
 import com.verix.sam.infrastructure.repository.BigQueryWriterRepository;
 import com.verix.sam.infrastructure.repository.CloudStorageRepository;
+import com.verix.sam.infrastructure.repository.model.SamTableSchema;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
 
 import java.util.HashMap;
@@ -33,7 +35,9 @@ public final class DIContainer {
                 .withValidation()
                 .as(JobOptions.class));
         container.register("cloud_storage_repository", new CloudStorageRepository(container.resolve("job_options")));
-        container.register("big_query_repository", new BigQueryWriterRepository(container.resolve("job_options")));
+        container.register("table_schema", new TableSchema());
+        container.register("sam_table_schema", new SamTableSchema(container.resolve("table_schema")));
+        container.register("big_query_repository", new BigQueryWriterRepository(container.resolve("job_options"), container.resolve("sam_table_schema")));
         container.register("reader_service", new ReaderService(container.resolve("cloud_storage_repository")));
         container.register("writer_service", new WriterService(container.resolve("big_query_repository")));
     }
