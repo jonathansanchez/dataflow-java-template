03515d7122f1561a2ad29d736c549d9516d4fa15
Jonathan Sánchez <jsanchezorellana@gmail.com>
2025-01-24 19:36:11 -0300

    feat: Dependency Injection Container was updated with Data Pipelines and dependencies services.

--- src/main/java/com/verix/sam/infrastructure/config/DIContainer.java
+++ src/main/java/com/verix/sam/infrastructure/config/DIContainer.java
@@ -6,6 +6,10 @@ import com.verix.sam.application.service.WriterService;
 import com.verix.sam.infrastructure.repository.BigQueryWriterRepository;
 import com.verix.sam.infrastructure.repository.CloudStorageRepository;
 import com.verix.sam.infrastructure.repository.model.SamTableSchema;
+import com.verix.sam.infrastructure.streaming.ApacheBeamDataPipeline;
+import com.verix.sam.infrastructure.streaming.transformation.RemoveLineBreaksTransformation;
+import com.verix.sam.infrastructure.streaming.transformation.StringToSamTransformation;
+import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
 
 import java.util.HashMap;
@@ -30,15 +34,30 @@ public final class DIContainer {
         PipelineOptionsFactory.register(JobOptions.class);
 
         DIContainer container = new DIContainer();
-        container.register("job_options", PipelineOptionsFactory
+        JobOptions options = PipelineOptionsFactory
                 .fromArgs(args)
                 .withValidation()
-                .as(JobOptions.class));
+                .as(JobOptions.class);
+
+        options.setInput("gs://bucket-swc-test/Sources/Landing/20250110/minilanding.csv");
+        options.setOutput("../Datos/modified_minilandingGCS");
+        options.setOutputTable("dataset_swc_test.sam");
+        options.setTempBucket("gs://bucket-swc-test/temp-files/sam");
+
+        container.register("job_options", options);
         container.register("cloud_storage_repository", new CloudStorageRepository(container.resolve("job_options")));
         container.register("table_schema", new TableSchema());
         container.register("sam_table_schema", new SamTableSchema(container.resolve("table_schema")));
         container.register("big_query_repository", new BigQueryWriterRepository(container.resolve("job_options"), container.resolve("sam_table_schema")));
+        container.register("pipeline", Pipeline.create(container.resolve("job_options")));
         container.register("reader_service", new ReaderService(container.resolve("cloud_storage_repository")));
         container.register("writer_service", new WriterService(container.resolve("big_query_repository")));
+        container.register("streaming_service", new WriterService(container.resolve("apache_beam_pipeline")));
+        container.register("remove_line_breaks_transformation", new RemoveLineBreaksTransformation());
+        container.register("string_to_sam_transformation", new StringToSamTransformation());
+        container.register("apache_beam_pipeline", new ApacheBeamDataPipeline(container.resolve("pipeline"), container.resolve("writer_service"), container.resolve("remove_line_breaks_transformation"), container.resolve("string_to_sam_transformation")));
+
+        //Init
+        container.resolve("apache_beam_pipeline");
     }
 }
