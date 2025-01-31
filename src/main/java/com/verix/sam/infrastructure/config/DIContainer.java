99fd223a2d1acc1febc4ef4dfe0ebda67e27664e
Jonathan Sánchez <jsanchezorellana@gmail.com>
2025-01-31 11:44:50 -0300

    refactor: Removed unused classes.

--- src/main/java/com/verix/sam/infrastructure/config/DIContainer.java
+++ src/main/java/com/verix/sam/infrastructure/config/DIContainer.java
@@ -1,11 +1,8 @@
 package com.verix.sam.infrastructure.config;
 
 import com.google.api.services.bigquery.model.TableSchema;
-import com.verix.sam.application.service.ReaderService;
 import com.verix.sam.application.service.StreamingService;
-import com.verix.sam.application.service.WriterService;
 import com.verix.sam.infrastructure.repository.BigQueryRepository;
-import com.verix.sam.infrastructure.repository.CloudStorageRepository;
 import com.verix.sam.infrastructure.repository.model.SamTableSchema;
 import com.verix.sam.infrastructure.streaming.ApacheBeamDataPipeline;
 import com.verix.sam.infrastructure.streaming.transformation.RemoveLineBreaksTransformation;
@@ -42,14 +39,10 @@ public final class DIContainer {
                 .as(JobOptions.class);
 
         container.register("job_options", options);
-        container.register("cloud_storage_repository", new CloudStorageRepository(container.resolve("job_options")));
         container.register("table_schema", new TableSchema());
         container.register("sam_table_schema", new SamTableSchema(container.resolve("table_schema")));
-        //container.register("big_query_repository", new BigQueryWriterRepository(container.resolve("job_options"), container.resolve("sam_table_schema")));
         container.register("big_query_repository", new BigQueryRepository(container.resolve("job_options"), container.resolve("sam_table_schema")));
         container.register("pipeline", Pipeline.create(container.resolve("job_options")));
-        container.register("reader_service", new ReaderService(container.resolve("cloud_storage_repository")));
-        container.register("writer_service", new WriterService(container.resolve("big_query_repository")));
         container.register("remove_line_breaks_transformation", new RemoveLineBreaksTransformation());
         container.register("string_to_sam_transformation", new StringToSamTransformation());
         container.register("sam_to_table_row_transformation", new SamToTableRow());
