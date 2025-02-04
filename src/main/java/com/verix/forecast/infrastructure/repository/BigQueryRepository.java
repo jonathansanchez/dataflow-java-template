49b09a7936b93ea248e7644aa2830d59dfa4c149
Jonathan Sánchez <jsanchezorellana@gmail.com>
2025-02-04 14:49:51 -0300

    feat: BigQuery repository was created with Schema Model.

--- src/main/java/com/verix/forecast/infrastructure/repository/BigQueryRepository.java
+++ src/main/java/com/verix/forecast/infrastructure/repository/BigQueryRepository.java
@@ -1,8 +1,8 @@
-package com.verix.sam.infrastructure.repository;
+package com.verix.forecast.infrastructure.repository;
 
 import com.google.api.services.bigquery.model.TableRow;
-import com.verix.sam.infrastructure.config.JobOptions;
-import com.verix.sam.infrastructure.repository.model.SamTableSchema;
+import com.verix.forecast.infrastructure.config.JobOptions;
+import com.verix.forecast.infrastructure.repository.model.RemediationTableSchema;
 import org.apache.beam.sdk.io.gcp.bigquery.BigQueryIO;
 import org.apache.beam.sdk.options.ValueProvider;
 import org.checkerframework.checker.initialization.qual.Initialized;
@@ -11,20 +11,19 @@ import org.checkerframework.checker.nullness.qual.UnknownKeyFor;
 
 public class BigQueryRepository {
     private final JobOptions options;
-    private final SamTableSchema samTableSchema;
+    private final RemediationTableSchema remediationTableSchema;
 
-    public BigQueryRepository(JobOptions options, SamTableSchema samTableSchema) {
-        this.options        = options;
-        this.samTableSchema = samTableSchema;
+    public BigQueryRepository(JobOptions options, RemediationTableSchema remediationTableSchema) {
+        this.options  = options;
+        this.remediationTableSchema = remediationTableSchema;
     }
 
     public BigQueryIO.Write<@UnknownKeyFor @NonNull @Initialized TableRow> writeToBigQuery() {
         return BigQueryIO.writeTableRows()
                 .to(options.getOutput())
-                .withSchema(samTableSchema.create())
+                .withSchema(remediationTableSchema.create())
                 .withCustomGcsTempLocation(ValueProvider.StaticValueProvider.of(options.getTempLocation()))
-                //.withWriteDisposition(BigQueryIO.Write.WriteDisposition.WRITE_TRUNCATE)
-                .withWriteDisposition(BigQueryIO.Write.WriteDisposition.WRITE_APPEND)
+                .withWriteDisposition(BigQueryIO.Write.WriteDisposition.WRITE_TRUNCATE)
                 .withCreateDisposition(BigQueryIO.Write.CreateDisposition.CREATE_IF_NEEDED);
     }
 }
---
+++ src/main/java/com/verix/forecast/infrastructure/repository/model/RemediationTableSchema.java
@@ -0,0 +1,53 @@
+package com.verix.forecast.infrastructure.repository.model;
+
+import com.google.api.services.bigquery.model.TableFieldSchema;
+import com.google.api.services.bigquery.model.TableSchema;
+
+import java.util.Arrays;
+
+import static com.google.cloud.bigquery.storage.v1.TableFieldSchema.Type.DATE;
+import static com.google.cloud.bigquery.storage.v1.TableFieldSchema.Type.STRING;
+
+public class RemediationTableSchema {
+    private final TableSchema schema;
+
+    public RemediationTableSchema(TableSchema schema) {
+        this.schema = schema;
+    }
+
+    public TableSchema create() {
+        return schema
+                .setFields(
+                        Arrays.asList(
+                                new TableFieldSchema()
+                                        .setName("strategy")
+                                        .setType(STRING.name())
+                                        .setMode("REQUIRED"),
+                                new TableFieldSchema()
+                                        .setName("apm_code")
+                                        .setType(STRING.name())
+                                        .setMode("REQUIRED"),
+                                new TableFieldSchema()
+                                        .setName("component")
+                                        .setType(STRING.name())
+                                        .setMode("REQUIRED"),
+                                new TableFieldSchema()
+                                        .setName("version")
+                                        .setType(STRING.name())
+                                        .setMode("REQUIRED"),
+                                new TableFieldSchema()
+                                        .setName("action")
+                                        .setType(STRING.name())
+                                        .setMode("REQUIRED"),
+                                new TableFieldSchema()
+                                        .setName("new_version")
+                                        .setType(STRING.name())
+                                        .setMode("REQUIRED"),
+                                new TableFieldSchema()
+                                        .setName("delivery_date")
+                                        .setType(DATE.name())
+                                        .setMode("REQUIRED")
+                        )
+                );
+    }
+}
