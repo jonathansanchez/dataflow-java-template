2c0926a9d8623ad0bb24c9ffd1f8c3b76ce7f7f6
shalinihenna <shalini.ramchandani@usach.cl>
2025-02-05 16:46:48 -0300

    test: StreamingServiceTest completed.

--- src/test/java/com/verix/landing/application/service/StreamingServiceTest.java
+++ src/test/java/com/verix/landing/application/service/StreamingServiceTest.java
@@ -3,11 +3,17 @@ package com.verix.landing.application.service;
 import com.verix.landing.domain.model.DataPipeline;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import static org.junit.jupiter.api.Assertions.*;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
 public class StreamingServiceTest {
 
-    /*@Mock
+    @Mock
     private DataPipeline dataPipeline;
 
     @InjectMocks
@@ -16,19 +22,26 @@ public class StreamingServiceTest {
     @Test
     void Given_a_pipeline_service_When_try_to_run_Then_call_pipeline_run(){
         //Arrange
-        doNothing().when(dataPipeline).run;
+        doNothing().when(dataPipeline).run();
 
         //Act
         streamingService.execute();
 
         //Assert
-        verify(dataPipeline, times(1).run);
+        verify(dataPipeline, times(1)).run();
     }
 
     @Test
     void Given_a_pipeline_service_When_try_to_run_and_fail_Then_throw_an_exception(){
-        assertThrows(RuntimeException.class, () -> doThrow().when(dataPipeline).run);
-    }*/
+        //Arrange
+        assertThrows(RuntimeException.class, () -> doThrow().when(dataPipeline).run());
+
+        //Act
+        streamingService.execute();
+
+        //Assert
+        verify(dataPipeline, times(1)).run();
+    }
 
 
 }
