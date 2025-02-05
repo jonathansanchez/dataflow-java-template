cf29f8549550765488f7b4cb3742f7f470ff28bc
shalinihenna <shalini.ramchandani@usach.cl>
2025-02-05 15:22:47 -0300

    test: StreamingServiceTest WIP

---
+++ src/test/java/com/verix/landing/application/service/StreamingServiceTest.java
@@ -0,0 +1,34 @@
+package com.verix.landing.application.service;
+
+import com.verix.landing.domain.model.DataPipeline;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class StreamingServiceTest {
+
+    /*@Mock
+    private DataPipeline dataPipeline;
+
+    @InjectMocks
+    private StreamingService streamingService;
+
+    @Test
+    void Given_a_pipeline_service_When_try_to_run_Then_call_pipeline_run(){
+        //Arrange
+        doNothing().when(dataPipeline).run;
+
+        //Act
+        streamingService.execute();
+
+        //Assert
+        verify(dataPipeline, times(1).run);
+    }
+
+    @Test
+    void Given_a_pipeline_service_When_try_to_run_and_fail_Then_throw_an_exception(){
+        assertThrows(RuntimeException.class, () -> doThrow().when(dataPipeline).run);
+    }*/
+
+
+}
