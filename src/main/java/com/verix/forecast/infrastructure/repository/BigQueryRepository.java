3e8a1b9840e466fe0407b34e379f9aa03b6ffd1e
Jonathan Sánchez <jsanchezorellana@gmail.com>
2025-02-17 17:18:08 -0300

    feat: Repository with Portfolio Table Schema was created.

--- src/main/java/com/verix/forecast/infrastructure/repository/BigQueryRepository.java
+++ src/main/java/com/verix/forecast/infrastructure/repository/BigQueryRepository.java
@@ -2,26 +2,35 @@ package com.verix.forecast.infrastructure.repository;
 
 import com.google.api.services.bigquery.model.TableRow;
 import com.verix.forecast.infrastructure.config.JobOptions;
-import com.verix.forecast.infrastructure.repository.model.RemediationTableSchema;
+import com.verix.forecast.infrastructure.repository.model.PortfolioTableSchema;
 import org.apache.beam.sdk.io.gcp.bigquery.BigQueryIO;
 import org.apache.beam.sdk.options.ValueProvider;
 import org.checkerframework.checker.initialization.qual.Initialized;
 import org.checkerframework.checker.nullness.qual.NonNull;
 import org.checkerframework.checker.nullness.qual.UnknownKeyFor;
 
-public class BigQueryRepository {
-    private final JobOptions options;
-    private final RemediationTableSchema remediationTableSchema;
+import java.io.Serializable;
 
-    public BigQueryRepository(JobOptions options, RemediationTableSchema remediationTableSchema) {
+public class BigQueryRepository implements Serializable {
+    private final JobOptions options;
+    private final PortfolioTableSchema portfolioTableSchema;
+
+    public BigQueryRepository(JobOptions options, PortfolioTableSchema portfolioTableSchema) {
         this.options  = options;
-        this.remediationTableSchema = remediationTableSchema;
+        this.portfolioTableSchema = portfolioTableSchema;
+    }
+
+    public BigQueryIO.TypedRead<@UnknownKeyFor @NonNull @Initialized TableRow> readLanding() {
+        String query = String.format("SELECT country, apm_code, sw_name, eol_last_day FROM `%s` WHERE country = '%s'",
+                options.getInput(),
+                options.getCountry());
+        return BigQueryIO.readTableRows().fromQuery(query).usingStandardSql();
     }
 
     public BigQueryIO.Write<@UnknownKeyFor @NonNull @Initialized TableRow> writeToBigQuery() {
         return BigQueryIO.writeTableRows()
                 .to(options.getOutput())
-                .withSchema(remediationTableSchema.create())
+                .withSchema(portfolioTableSchema.create())
                 .withCustomGcsTempLocation(ValueProvider.StaticValueProvider.of(options.getTempLocation()))
                 .withWriteDisposition(BigQueryIO.Write.WriteDisposition.WRITE_TRUNCATE)
                 .withCreateDisposition(BigQueryIO.Write.CreateDisposition.CREATE_IF_NEEDED);
