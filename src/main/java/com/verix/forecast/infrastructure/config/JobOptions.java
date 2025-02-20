eb0e30ad1d0e511ab95f0428a4a69fc6dd391791
Jonathan Sánchez <jsanchezorellana@gmail.com>
2025-02-20 11:10:58 -0300

    feat: Job Options was updated with required parameters.

--- src/main/java/com/verix/forecast/infrastructure/config/JobOptions.java
+++ src/main/java/com/verix/forecast/infrastructure/config/JobOptions.java
@@ -5,12 +5,20 @@ import org.apache.beam.sdk.options.PipelineOptions;
 
 public interface JobOptions extends PipelineOptions {
     @Description("Input for the pipeline")
-    String getInput();
-    void setInput(String input);
+    String getInputFile();
+    void setInputFile(String input);
+
+    @Description("Input for the pipeline")
+    String getInputPortfolio();
+    void setInputPortfolio(String input);
 
     @Description("Output for the pipeline")
-    String getOutput();
-    void setOutput(String output);
+    String getOutputStrategy();
+    void setOutputStrategy(String output);
+
+    @Description("Output for the pipeline")
+    String getOutputForecast();
+    void setOutputForecast(String output);
 
     @Description("Temp bucket location")
     String getTemp();
