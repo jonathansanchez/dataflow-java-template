5d5c3f1cd2dd051e7f3cf4f791f24949c9157f54
Jonathan Sánchez <jsanchezorellana@gmail.com>
2025-02-20 11:04:05 -0300

    feat: Streaming Service was created for pipelines orchestration.

--- src/main/java/com/verix/forecast/application/service/StreamingService.java
+++ src/main/java/com/verix/forecast/application/service/StreamingService.java
@@ -1,15 +1,44 @@
 package com.verix.forecast.application.service;
 
-import com.verix.forecast.domain.model.DataPipeline;
+import com.verix.forecast.application.service.request.ForecastRequest;
+import com.verix.forecast.domain.model.DataPipelineStrategy;
+import com.verix.forecast.domain.model.Portfolio;
+import com.verix.forecast.domain.model.Remediation;
+import com.verix.forecast.domain.model.exception.InvalidCountryException;
+
+import java.util.List;
+import java.util.Optional;
 
 public class StreamingService {
-    private final DataPipeline pipeline;
+    private final DataPipelineStrategy pipeline;
+    private final ForecastService      forecastService;
 
-    public StreamingService(DataPipeline pipeline) {
-        this.pipeline = pipeline;
+    public StreamingService(DataPipelineStrategy pipeline, ForecastService forecastService) {
+        this.pipeline        = pipeline;
+        this.forecastService = forecastService;
     }
 
     public void execute() {
-        pipeline.run();
+        List<Remediation> remediationList = pipeline.runStrategy();
+        List<Portfolio>   portfolioList   = pipeline.runCurrentStateForecast(getCountryCode(remediationList));
+
+        forecastService.execute(
+                ForecastRequest.of(remediationList, portfolioList)
+        );
+    }
+
+    private String getCountryCode(List<Remediation> remediationList) {
+        Optional<String> value = Optional.ofNullable(remediationList
+                .stream()
+                .findFirst()
+                .get()
+                .getCountryCode()
+                .getValue());
+
+        if (value.isPresent()) {
+            return value.get();
+        }
+
+        throw InvalidCountryException.thrown();
     }
 }
